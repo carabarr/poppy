44a8278f6b5b978bbe0abe8da8159da86127ab4b
carabarr <cpatricia.ab@gmail.com>
2022-08-25 23:48:53 -0400

    added database uploading support

--- book_db.py
+++ book_db.py
@@ -1,5 +1,6 @@
 import requests
 import click
+import psycopg2
 
 @click.command()
 @click.argument('title')
@@ -21,22 +22,20 @@ def search(title, read, author):
                     f" by {', '.join(str(x) for x in first_result.get('author_name'))}")
     click.confirm(confirmation, abort=True)
 
-    # adds author and cover id data (harder to get from actual work page)
     parse_data(book_data, first_result)
-
-    # gets the unique key associated with the work for searching 
     work_key = first_result.get('key')
     work = search_by_work(work_key)
+    # description is the only thing missing from search result pages
     book_data['description'] = work.get('description').get('value')
-    
-    print(book_data)
+
+    upload_to_db(book_data)
 
 def parse_data(book_data, result):
     book_data['title'] = result.get('title')
     book_data['subtitle'] = result.get('subtitle')
     book_data['author'] = result.get('author_name')
+    book_data['genre'] = result.get('subject')
     book_data['cover_id'] = result.get('cover_i')
-
 
 def urlify_title(title, author):
     base = "https://openlibrary.org/search.json?"
@@ -52,6 +51,17 @@ def search_by_work(work):
     r = requests.get(url)
     data = r.json()
     return data
+
+def upload_to_db(book_data):
+    conn = psycopg2.connect('dbname=book_db user=claudia')
+    cur = conn.cursor()
+    cur.execute("""
+                INSERT INTO books (title, subtitle, author, genre, cover, description)
+                VALUES (%s, %s, %s, %s, %s, %s)
+                """, list(book_data.values()))
+    conn.commit()
+    cur.close()
+    conn.close()
 
 if __name__ == '__main__':
     search()
